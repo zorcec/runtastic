688ae5a3e788163df47a832399881d97a09f528b
zorcec <zorcec@gmail.com>
2014-04-26 18:21:47 +0200

    Task 3 - Mobile UI

---
+++ js/app.helpers.js
@@ -0,0 +1,9 @@
+﻿(function () {
+
+    // Registering a date-time helper
+    // This helper will help us with a date formatting
+    Ember.Handlebars.helper('format-date', function (date) {
+        return moment(date).format('DD.MM.YYYY, HH.mm');
+    });
+
+})();
